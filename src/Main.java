37fc81444f5f70ade4fa073c24832d87874e8759
olgatut <lelka.tutskaya@yandex.ru>
2022-10-09 20:17:10 +0300

    Домашнее задание CYCLE2 started

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
-    public static void main(String[] args) {
-
+    public static void main(String[] args){
+        int salary = 15000;
+        int total = 0;
+        while (total < 2_590_000){
+            total = total + salary;
+            System.out.println(total);
+        }
+    //15000
+    // 259000
+    //"Месяц … , сумма накоплений равна … рублей"
 
         System.out.println();
     }
